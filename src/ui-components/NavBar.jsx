c68f99f816a593939bde5d01ca8b04ac74a5a771
Matt Auerbach <tennisfan0526@gmail.com>
2022-07-09 22:07:16 -0400

    test

--- src/ui-components/NavBar.jsx
+++ src/ui-components/NavBar.jsx
@@ -6,10 +6,18 @@
 
 /* eslint-disable */
 import React from "react";
-import { getOverrideProps } from "@aws-amplify/ui-react/internal";
-import { Flex, Icon, Image, Text, View } from "@aws-amplify/ui-react";
+import {
+  getOverrideProps,
+  useAuth,
+  useAuthSignOutAction,
+} from "@aws-amplify/ui-react/internal";
+import { Flex, Image, Text } from "@aws-amplify/ui-react";
 export default function NavBar(props) {
   const { overrides, ...rest } = props;
+  const authAttributes = useAuth().user?.attributes ?? {};
+  const helloCommaMattExclamationMarkOnClick = useAuthSignOutAction({
+    global: true,
+  });
   return (
     <Flex
       gap="20px"
@@ -130,34 +138,27 @@ export default function NavBar(props) {
         padding="0px 0px 0px 0px"
         {...getOverrideProps(overrides, "Frame 32129767081")}
       >
-        <View
-          width="24px"
-          height="24px"
+        <Text
+          fontFamily="Inter"
+          fontSize="27px"
+          fontWeight="400"
+          color="rgba(255,255,255,1)"
+          lineHeight="24px"
+          textAlign="left"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          letterSpacing="0.12px"
           shrink="0"
-          overflow="hidden"
           position="relative"
           padding="0px 0px 0px 0px"
-          {...getOverrideProps(overrides, "Icon")}
-        >
-          <Icon
-            width="16px"
-            height="19.5px"
-            viewBox={{ minX: 0, minY: 0, width: 16, height: 19.5 }}
-            paths={[
-              {
-                d: "M8 19.5C9.1 19.5 10 18.6 10 17.5L6 17.5C6 18.6 6.9 19.5 8 19.5ZM14 13.5L14 8.5C14 5.43 12.37 2.86 9.5 2.18L9.5 1.5C9.5 0.67 8.83 0 8 0C7.17 0 6.5 0.67 6.5 1.5L6.5 2.18C3.64 2.86 2 5.42 2 8.5L2 13.5L0 15.5L0 16.5L16 16.5L16 15.5L14 13.5ZM12 14.5L4 14.5L4 8.5C4 6.02 5.51 4 8 4C10.49 4 12 6.02 12 8.5L12 14.5Z",
-                fill: "rgba(51,51,51,1)",
-                fillRule: "nonzero",
-              },
-            ]}
-            position="absolute"
-            top="10.42%"
-            bottom="8.33%"
-            left="16.67%"
-            right="16.67%"
-            {...getOverrideProps(overrides, "Vector")}
-          ></Icon>
-        </View>
+          whiteSpace="pre-wrap"
+          children={authAttributes["name"]}
+          onClick={() => {
+            helloCommaMattExclamationMarkOnClick();
+          }}
+          {...getOverrideProps(overrides, "Hello, Matt!")}
+        ></Text>
         <Image
           width="45px"
           height="45px"
